031edb65a7647f3c0761a73454739a869f59a296
Prayush Hada <reachprayush@gmail.com>
2023-12-08 09:10:56 +0545

    Previous State

--- script.js
+++ script.js
@@ -8,112 +8,67 @@ const five = document.getElementById("five");
 const range = 10;
 
 function ek() {
-	// Check if content is already present
-	const existingContent = one.textContent.trim();
+	for(let i = 1; i <= range; i++){
+		const result = 1 * i;
+		//Create a new paragraph element to display the table
+		const paragraph = document.createElement("p");
+		paragraph.textContent = '1 x ' + i + " = " + result;
+
+		//Append the paragraph to the Id "one"
+		one.appendChild(paragraph);
 	
-	// If content is present, clear it
-	if (existingContent) {
-	  one.textContent = '';
-	  return;
 	}
-  
-	// If content is not present, generate and append the table
-	for (let i = 1; i <= range; i++) {
-	  const result = 1 * i;
-	  // Create a new paragraph element to display the table
-	  const paragraph = document.createElement("p");
-	  paragraph.textContent = '1 x ' + i + " = " + result;
-  
-	  // Append the paragraph to the Id "one"
-	  one.appendChild(paragraph);
-	}
-  }
-  
+	
+}
 
 function dui() {
-	// Check if content is already present
-	const existingContent = one.textContent.trim();
-	
-	// If content is present, clear it
-	if (existingContent) {
-	  one.textContent = '';
-	  return;
-	}
-  
-	// If content is not present, generate and append the table
-	for (let i = 1; i <= range; i++) {
-	  const result = 2 * i;
-	  // Create a new paragraph element to display the table
-	  const paragraph = document.createElement("p");
-	  paragraph.textContent = '2 x ' + i + " = " + result;
-  
-	  // Append the paragraph to the Id "one"
-	  one.appendChild(paragraph);
+	for(let i = 1; i <= range; i++){
+		const result = 2 * i;
+		//Create a new paragraph element to display the table
+		const paragraph = document.createElement("p");
+		paragraph.textContent = '2 x ' + i + " = " + result;
+
+		//Append the paragraph to the Id "one"
+		two.appendChild(paragraph);
+
 	}
 }
 
 function tin() {
-	// Check if content is already present
-	const existingContent = one.textContent.trim();
-	
-	// If content is present, clear it
-	if (existingContent) {
-	  one.textContent = '';
-	  return;
-	}
-  
-	// If content is not present, generate and append the table
-	for (let i = 1; i <= range; i++) {
-	  const result = 3 * i;
-	  // Create a new paragraph element to display the table
-	  const paragraph = document.createElement("p");
-	  paragraph.textContent = '3 x ' + i + " = " + result;
-  
-	  // Append the paragraph to the Id "one"
-	  one.appendChild(paragraph);
+	for(let i = 1; i <= range; i++){
+		const result = 3 * i;
+		//Create a new paragraph element to display the table
+		const paragraph = document.createElement("p");
+		paragraph.textContent = '3 x ' + i + " = " + result;
+
+		//Append the paragraph to the Id "one"
+		three.appendChild(paragraph);
+
 	}
 }
 
 function char() {
-	// Check if content is already present
-	const existingContent = one.textContent.trim();
-	
-	// If content is present, clear it
-	if (existingContent) {
-	  one.textContent = '';
-	  return;
-	}
-  
-	// If content is not present, generate and append the table
-	for (let i = 1; i <= range; i++) {
-	  const result = 4 * i;
-	  // Create a new paragraph element to display the table
-	  const paragraph = document.createElement("p");
-	  paragraph.textContent = '4 x ' + i + " = " + result;
-  
-	  // Append the paragraph to the Id "one"
-	  one.appendChild(paragraph);
+	for(let i = 1; i <= range; i++){
+		const result = 4 * i;
+		//Create a new paragraph element to display the table
+		const paragraph = document.createElement("p");
+		paragraph.textContent = '4 x ' + i + " = " + result;
+
+		//Append the paragraph to the Id "one"
+		four.appendChild(paragraph);
+
 	}
 }
 
 function pach() {
-	// Check if content is already present
-	const existingContent = one.textContent.trim();
-	
-	// If content is present, clear it
-	if (existingContent) {
-	  one.textContent = '';
-	  return;
-	}
-  
-	// If content is not present, generate and append the table
-	for (let i = 1; i <= range; i++) {
-	  const result = 5 * i;
-	  // Create a new paragraph element to display the table
-	  const paragraph = document.createElement("p");
-	  paragraph.textContent = '5 x ' + i + " = " + result;
-  
-	  // Append the paragraph to the Id "one"
-	  one.appendChild(paragraph);
+	for(let i = 1; i <= range; i++){
+		const result = 5 * i;
+		//Create a new paragraph element to display the table
+		const paragraph = document.createElement("p");
+		paragraph.textContent = '5 x ' + i + " = " + result;
+
+		//Append the paragraph to the Id "one"
+		five.appendChild(paragraph);
+
 	}
 }
